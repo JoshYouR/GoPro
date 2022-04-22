897aab46b070090469bc9f8473f76f835b9fb629
JoshYouR <56334827+JoshYouR@users.noreply.github.com>
2022-04-22 11:53:11 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Basic CLI for extracting gpx data from GoPro Videos
+### Installation
+Install with `npm -i -g extract-goprogpx`.
+Global installation is necessary for the CLI to be used in any directory in the terminal.
+
+### Usage
+In terminal shell, use `extract-goprogpx -input [path/to/video-name] -output [path/to/file-name]`.
+Alternatively, use `-i` instead of `-input`, `-o` instead of `-output`.
+> Don't add the extension to the `-output` name.
+
+### Credit
+ffmpeg
+fluent-ffmpeg/node-fluent-ffmpeg
+ffprobe
+ScottyFillups/ffprobe-client
+JuanIrache/gopro-telemetry
+yargs/yargs
---
+++ bin/index.js
@@ -0,0 +1,70 @@
+#! /usr/bin/env node
+
+const ffmpeg = require('fluent-ffmpeg');
+const ffprobe = require('ffprobe-client');
+const _goproTelemetry = require('gopro-telemetry');
+const {writeFile: _writeFile} = require('fs/promises');
+const yargs = require('yargs');
+
+const pipe = fns => x => fns.reduce((f, g) => g(f), x);
+
+const get = string => object => object[string];
+
+const {input, output} = yargs.usage('extract-goprogpx [args]')
+.option('input', {alias: 'i', describe: 'Set the input file path', type: 'string', demandOption: true})
+.option('output', {alias: 'o', describe: 'Set the output file path. Don\' include extension.', type: 'string', demandOption: true})
+.argv;
+
+const goproTelemetry = options => data => new Promise((res, rej) => _goproTelemetry(data, options, telemetry => res(telemetry)));
+
+const writeFile = path => data => _writeFile(path, data);
+
+const pipeP = promises => x => promises.reduce((f, g) => f.then(g), Promise.resolve(x));
+
+const getCharAtIndex = index => string => string.charAt(index);
+
+const getFirstChar = getCharAtIndex(0);
+
+const getStreams = get('streams');
+
+const getCodecTagString = get('codec_tag_string');
+
+const getIndex = get('index');
+
+const getId = pipe([
+    get('id'),
+    getFirstChar
+]);
+
+const find = predicate => array => array.find(predicate);
+
+const isGpmdStream = stream => getCodecTagString(stream) === 'gpmd';
+
+const findGoProMetaDataStream = find(isGpmdStream);
+
+const getGpmdStreamIndex = url => pipeP([
+    ffprobe,
+    getStreams,
+    findGoProMetaDataStream,
+    stream => ({index: getIndex(stream), id: getId(stream)})
+])(url);
+
+const createGPSBinary = url => async ({index, id}) => {
+                let buff = Buffer.alloc(0);
+                return new Promise((res, rej) => ffmpeg(url)
+                    .outputOptions(['-y', '-c copy', `-map ${id}:${index}`, '-f rawvideo'])
+                    .on('error', rej)
+                    .pipe()
+                    .on('data', data => {buff = Buffer.concat([buff, data])})
+                    .on('end', res)
+                    ).then(() => ({rawData: buff}));
+};
+
+const run = inputPath => outputPath => pipeP([
+    getGpmdStreamIndex,
+    createGPSBinary(inputPath),
+    goproTelemetry({preset: 'gpx'}),
+    writeFile(`${outputPath}.gpx`)
+])(inputPath);
+
+run(input)(output);
